f14dc3866738a62d6e5716301468b1fea0562ef4
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-03-05 08:08:03 +1000

    adding bindings project

---
+++ MDCParallaxView.Bindings/ApiDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MDCParallaxView.Bindings
+{
+	[BaseType (typeof (UIView))]
+	public partial interface MDCParallaxView {
+
+		[Export ("scrollView")]
+		UIScrollView ScrollView { get; }
+
+		[Export ("scrollViewDelegate", ArgumentSemantic.Assign)]
+		UIScrollViewDelegate ScrollViewDelegate { get; set; }
+
+		[Export ("backgroundHeight")]
+		float BackgroundHeight { get; set; }
+
+		[Export ("backgroundInteractionEnabled")]
+		bool BackgroundInteractionEnabled { [Bind ("isBackgroundInteractionEnabled")] get; set; }
+
+		[Export ("initWithBackgroundView:foregroundView:")]
+		IntPtr Constructor (UIView backgroundView, UIView foregroundView);
+	}
+}
+
