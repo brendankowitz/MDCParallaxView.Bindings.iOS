69c1a96a157bf8739e7c65e24cfa40f96754b3f5
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-03-05 08:08:28 +1000

    updating link

--- MDCParallaxView.Bindings/libParallaxViewSDK.linkwith.cs
+++ MDCParallaxView.Bindings/libParallaxViewSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libParallaxViewSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libParallaxViewSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "Foundation")]
